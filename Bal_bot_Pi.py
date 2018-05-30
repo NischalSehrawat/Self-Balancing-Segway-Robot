1cbd27feb730d44a78a74d365aa6caa72dab9586
SEHN <nischal.sehrawat@witteveenbos.com>
2018-05-30 15:55:34 +0200

    Added new required libraries / code
    Added new required libraries / code

--- Bal_bot_Pi.py
+++ Bal_bot_Pi.py
@@ -1,19 +1,21 @@
 from numpy import arctan
-import matplotlib.pyplot as plt
-from datetime import datetime
 from time import sleep
 from mpu6050 import mpu6050
 from gpiozero import Robot
+from My_PID import PID
 
+cont = PID(Kp = 0, Kd = 0 , Ki = 0, 
+           SetPoint = 0, SampleTime = 200, OutMin = 30, OutMax = 255, mode = "Auto") # Initialise PID controller
 
-sensor = mpu6050(0x68)
+arduino = serial.Serial('COM3', baudrate = 115200) # Initialise arduino serial for reading Kp, Kd, and Ki analogue input from the potentiometer
+
+mympu = mpu6050(0x68)
 
 robot = Robot(left=(4, 14), right=(17, 27)) # Initialise robot object and its pin numbers
 
+A_Y, A_Z = mympu.get_accel_data()['y'], mympu.get_accel_data()['z'] # Acceleration data
 
-A_Y, A_Z = sensor.get_accel_data()['y'], sensor.get_accel_data()['z'] # Acceleration data
-
-omega_x = sensor.get_gyro_data()['x'] # Gyro data
+omega_x = mympu.get_gyro_data()['x'] # Gyro data
 
 roll = (arctan(A_Y/A_Z ))*57.32; # Rotation about X axis [deg]
 
