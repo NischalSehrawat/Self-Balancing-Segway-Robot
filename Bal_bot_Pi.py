022142e641bb8f384b47e8a0f9c2747ec31de0f5
SEHN <nischal.sehrawat@witteveenbos.com>
2018-05-31 17:20:28 +0200

    Added code to calculate angle from MPU data
    Added code to calculate angle from MPU data

--- Bal_bot_Pi.py
+++ Bal_bot_Pi.py
@@ -1,5 +1,6 @@
-from numpy import arctan
+from numpy import arctan, deg2rad
 from time import sleep
+from datetime import datetime
 from mpu6050 import mpu6050
 from gpiozero import Robot, DistanceSensor
 from My_PID import PID
@@ -33,8 +34,36 @@ robot = Robot(left=(l_mot_1, l_mot_2), right=(r_mot_1, r_mot_2)) # Initialise ro
 
 a_y, a_z = my_mpu.get_accel_data()['y'], my_mpu.get_accel_data()['z'] # Acceleration data
 
-omega_x = my_mpu.get_gyro_data()['x'] # Gyro data
+Theta_x = arctan(a_y/a_z ); # Rotation about X axis [rad]
 
-roll = arctan(a_y/a_z ); # Rotation about X axis [rad]
+t_start = datetime.now() # Timer for gyro unit
 
+alpha = 0.98; # Complimentary filter control parameter
 
+sleep(1)
+
+while 1:
+    
+    omega_x = my_mpu.get_gyro_data()['x'] # Gyro data
+    
+    t_now = datetime.now() # Log what time it is now
+    
+    dt_gyro = (t_now - t_start).total_seconds() # Time difference for gyro angle calculations
+    
+    a_y, a_z = my_mpu.get_accel_data()['y'], my_mpu.get_accel_data()['z'] # Acceleration data
+    
+    roll = arctan(a_y/a_z) # Angle calculated by accelerometer readings about X axis in [rad]
+  
+    Theta_x = alpha*(Theta_x + deg2rad(omega_x * dt_gyro)) + (1-alpha)*roll # Calculate the total angle using a Complimentary filter
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    t_start = t_now # Store the previous time variable for calculating "dt_gyro"  
+    
+    
