c9c4af7768a40e878f166540060b6a93f160d53f
SEHN <nischal.sehrawat@witteveenbos.com>
2018-07-16 10:28:41 +0200

    Added code for motor RPM and MPU6050 data
    Added new classes for calcuating angles and motor RPM

--- Bal_bot_Pi.py
+++ Bal_bot_Pi.py
@@ -1,4 +1,4 @@
-from numpy import arctan, deg2rad
+from numpy import floor, ceil, arctan, deg2rad, rad2deg, arange, array, mean
 from time import sleep
 from datetime import datetime
 from mpu6050 import mpu6050
@@ -7,63 +7,193 @@ from My_PID import PID
 from serial import Serial
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD) # Initialize the GPIO to use the BOARD pin numbering schemes
+#%% =================IMPORTANT NOTE===================
+'''
+gpiozero library uses Broadcom (BCM) pin numbering
+for the GPIO pins, as opposed to physical (BOARD)
+numbering. Unlike in the RPi.GPIO library,
+this is not configurable.
+'''
+GPIO.setmode(GPIO.BCM) # Initialize the GPIO to use the BCM pin numbering schemes as
+                       # gpiozero uses BCM and is not configurable
 
-#%%=============== Setup motor control + encoder pins to output / input ==================
+#====SETUP MOTOR CONTROL PINS ========
 
-l_mot_1 = 11; GPIO.setup(l_mot_1,GPIO.OUT); l_mot_2 = 12; GPIO.setup(l_mot_2,GPIO.OUT)
+s = 0.05
 
-r_mot_1 = 17; GPIO.setup(r_mot_1,GPIO.OUT); r_mot_2 = 15; GPIO.setup(r_mot_2,GPIO.OUT)
+l_mot_1 = 6; GPIO.setup(l_mot_1,GPIO.OUT);
+l_mot_2 = 13; GPIO.setup(l_mot_2,GPIO.OUT)
+
+r_mot_1 = 19; GPIO.setup(r_mot_1,GPIO.OUT);
+r_mot_2 = 26; GPIO.setup(r_mot_2,GPIO.OUT)
 
 #=========== SETUP ENCODER PINS ===============
 
-enc_L_1 = 22; GPIO.setup(enc_L_1,GPIO.IN); enc_L_2 = 25; GPIO.setup(enc_L_2,GPIO.IN);
+enc_L = 4; tick_L = 0;  # Left motor 
 
-enc_R_1 = 29; GPIO.setup(enc_R_1,GPIO.IN); enc_R_2 = 30; GPIO.setup(enc_R_2,GPIO.IN);
+enc_R = 17; tick_R = 0; # Right motor
 
-#%% ===============================================================
+ppr = 990; RPM_limit = 1 # RPM below which we read "0"
 
-cont = PID(Kp = 0, Kd = 0 , Ki = 0, 
-           SetPoint = 0, SampleTime = 200, OutMin = 0.10, OutMax = 1, mode = "Auto") # Initialise PID controller
+num_avg_point = 10 # Number of points to be used for exponential avergaing and smoothing out RPM data 
 
-arduino = Serial('COM3', baudrate = 115200) # Initialise arduino serial for reading Kp, Kd, and Ki analogue input from the potentiometer
+beta = 1- 1/num_avg_point # parameter used for exponential averaging
+
+alpha = 0.985; # Complimentary filter control parameter
+
+sleep(2)
+
+def my_callback_R(channel):
+    global tick_R
+    tick_R+=1
+  
+def my_callback_L(channel):
+    global tick_L
+    tick_L+=1
+    
+GPIO.setup(enc_L, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set Pull left encoder pin to up 
+
+GPIO.add_event_detect(enc_L, GPIO.RISING, callback=my_callback_L) # Set the interrupt functions for left motor
+
+GPIO.setup(enc_R, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set Pull right encoder pin to up
+
+GPIO.add_event_detect(enc_R, GPIO.RISING, callback=my_callback_R) # Set the interrupt functions for right motor
+
+class my_motors:
+    
+    def __init__(self, beta, ppr, position, RPM_limit):
+
+        self.t_prev = datetime.now() # Time during object initialisation
+
+        self.beta = beta; # parameter used for exponential averaging
+        
+        self.RPM_prev = 0; # Starting RPM value
+        
+        self.n_prev = 0; # Starting encoder count
+        
+        self.ppr = ppr # Number of pulses generated per revolution
+        
+        self.position = position # motor position "L" or "R"
+        
+        self.RPM_limit = RPM_limit # Below this RPM, send RPM = 0
+    
+    def get_RPM(self): # Get motor RPM 
+        
+        global tick_L, tick_R
+        
+        if self.position == "L":
+            
+            n_now = tick_L
+        
+        elif self.position == "R":
+            
+            n_now = tick_R            
+        
+        t_now = datetime.now() # Find what time is it now
+        
+        delta_t = t_now - self.t_prev  # Calculate time difference between consecutive readings 
+        
+        dt = delta_t.total_seconds() # Total time difference in seconds
+
+        dn = n_now - self.n_prev # Difference between motor encoder counts
+        
+        # Calculate RPMs and smooth out the data usinge exponential averaging
+
+        RPM_now = ceil(self.beta * (dn / dt)*(60.0 / self.ppr) + (1 - self.beta) *  self.RPM_prev)
+        
+        if RPM_now < self.RPM_limit:
+            
+            RPM_now = 0 # If RPM is less than the limiting RPM, make it "0"
+            
+        RPM_dot = (RPM_now - self.RPM_prev) / dt # Time derivative of omega
+        
+        self.t_prev = t_now # Make previous time equal to present time
+        
+        self.n_prev = n_now # Make previous encoder counts equal to present encoder counts 
+        
+        self.RPM_prev = RPM_now # Make previous RPM equal to present RPM
+        
+        return RPM_now, RPM_dot
+    
+class my_accel:
+    
+    def __init__(self, my_mpu, alpha):
+
+        self.my_mpu = my_mpu
+        self.t_prev = datetime.now() # Time during object initialisation
+        self.alpha = alpha # parameter for controlling Gyro and accelerometer contribution        
+        self.Theta_x = arctan(self.my_mpu.get_accel_data()['y']/self.my_mpu.get_accel_data()['z']); # Initial rotation about X axis [rad] 
+        self.theta_init = self.Theta_x # Used for calculating theta_dot
+
+        # We need to caliberate the MPU for errors
+
+        calib = []
+
+        print("Starting MPU caliberation..."); sleep(2)
+
+        for i in range(100):
+
+            xx = [0-self.my_mpu.get_accel_data()['y'], 9.8-self.my_mpu.get_accel_data()['z'],
+                  0-self.my_mpu.get_gyro_data()['x']]
+            
+            calib.append(xx)
+
+        self.calib = mean(array(calib), axis = 0)
+
+        print("MPU  caliberated, corrections Y, Z, Omega_x = ", round(self.calib[0], 2), round(self.calib[1], 2), round(self.calib[2], 2))
+
+        sleep(2)
+        
+        
+    def get_Theta_X(self): # Get tilt angle  
+        
+        omega_x = self.my_mpu.get_gyro_data()['x'] + self.calib[2]  # Gyro data
+        
+        t_now = datetime.now() # Find what time is it now
+        
+        delta_t = t_now - self.t_prev  # Calculate time difference between consecutive readings 
+        
+        dt = delta_t.total_seconds() # Total time difference in seconds
+        
+        roll = arctan((self.my_mpu.get_accel_data()['y'] + self.calib[0]) / (self.my_mpu.get_accel_data()['z'] + self.calib[1]))
+        
+        self.Theta_x = self.alpha*(self.Theta_x + deg2rad(omega_x * dt)) + (1-self.alpha)*roll # Calculate the total angle using a Complimentary filter
+              
+        theta_dot = (self.Theta_x - self.theta_init) / dt # Time derivative of theta
+        
+        self.theta_init = self.Theta_x # Make previous angle equal to new angle for calculating derivative
+        
+        self.t_prev = t_now # Store the previous time variable for calculating "dt_gyro" 
+        
+        return rad2deg(self.Theta_x), rad2deg(theta_dot)
+
+
+#====================MAIN COMPUTING LOOP ===================
+
+#cont = PID(Kp = 0, Kd = 0 , Ki = 0, 
+           #SetPoint = 0, SampleTime = 200, OutMin = 0.10, OutMax = 1, mode = "Auto") # Initialise PID controller
+
+#arduino = Serial('COM3', baudrate = 115200) # Initialise arduino serial for reading Kp, Kd, and Ki analogue input from the potentiometer
+
+my_mpu = mpu6050(0x68) # Initialize MPU to get acceleration and rotation data
+    
+left_motor = my_motors(beta, ppr, "L", RPM_limit) # Class / object to get RPM data
+
+right_motor = my_motors(beta, ppr, "R", RPM_limit) # Class / object to get RPM data
 
 my_mpu = mpu6050(0x68) # Initialize MPU to get acceleration and rotation data
 
-robot = Robot(left=(l_mot_1, l_mot_2), right=(r_mot_1, r_mot_2)) # Initialise robot object and its pin numbers
+My_Mpu = my_accel(my_mpu, alpha) # Object for getting theta and theta_dot
 
-a_y, a_z = my_mpu.get_accel_data()['y'], my_mpu.get_accel_data()['z'] # Acceleration data
+my_robot = Robot(left=(l_mot_1, l_mot_2), right=(r_mot_1, r_mot_2)) # Initialise robot object and its pin numbers
 
-Theta_x = arctan(a_y/a_z ); # Rotation about X axis [rad]
+my_robot.stop()
 
-t_start = datetime.now() # Timer for gyro unit
+sleep(2)
 
-alpha = 0.98; # Complimentary filter control parameter
-
-sleep(1)
+print("Starting the system..!!!")
 
 while 1:
-    
-    omega_x = my_mpu.get_gyro_data()['x'] # Gyro data
-    
-    t_now = datetime.now() # Log what time it is now
-    
-    dt_gyro = (t_now - t_start).total_seconds() # Time difference for gyro angle calculations
-    
-    a_y, a_z = my_mpu.get_accel_data()['y'], my_mpu.get_accel_data()['z'] # Acceleration data
-    
-    roll = arctan(a_y/a_z) # Angle calculated by accelerometer readings about X axis in [rad]
-  
-    Theta_x = alpha*(Theta_x + deg2rad(omega_x * dt_gyro)) + (1-alpha)*roll # Calculate the total angle using a Complimentary filter
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    t_start = t_now # Store the previous time variable for calculating "dt_gyro"  
-    
-    
+    a, _ = My_Mpu.get_Theta_X()
+
+    print("Current angle = ", ceil(a))
