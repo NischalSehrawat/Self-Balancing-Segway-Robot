6d5bd27b469d03caaacad1358c36013735a5b374
SEHN <nischal.sehrawat@witteveenbos.com>
2018-07-27 11:19:08 +0200

    Searched and added LQR function
    Searched and added LQR function

--- Parameters_and_matrices.py
+++ Parameters_and_matrices.py
@@ -3,80 +3,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import  scipy.io
-from control import *
-
+import scipy.linalg
+from scipy.integrate import odeint
 
 #%% parameters 
 
-m_wh = 0.058; # MAss of one wheel [kg]
+t_sim = 5; # Total simulation time [s]
 
-m_mot = 0.2 # Mass of 1 motor stator [Kg]
+m_wh = 0.058 + 0.2; # MAss of one wheel + mass of 1 motor [kg]
 
-r = 0.5 * 0.085 # Wheel radius [m]
+r = 0.5 * 0.085; # Wheel radius [m]
 
-b = 0.255 # Chassis length (along the axle) [m]
+b = 0.255; # Chassis length (along the axle) [m]
 
-a = 0.1 # Chassis width (perpendicular to the axle) [m]
+a = 0.1; # Chassis width (perpendicular to the axle) [m]
 
-m_plat = 0.081 # mass of 1 wooden platform [kg]
+m_plat = 0.081; # mass of 1 wooden platform [kg]
 
-m_bolt = 0.03 # Mass of 1 bolt [kg]
+m_bolt = 0.03; # Mass of 1 bolt [kg]
 
-m_0 = 2*m_mot + 2*m_plat + 4*m_bolt # total Mass of the body [Kg]
+m_0 = 2*m_plat + 4*m_bolt; # total Mass of the body / chassis (excluding wheel and motors) [Kg]
 
-d1 = 0.0325 # distance of the axle from the lower woodden platform [m]
+d1 = 0.0325; # distance of the axle from the lower woodden platform [m]
 
 L1 = 0.15; # Length of 1 bolt [m]
 
-L_diag = (a**2 + b**2)**0.5 # Digonal distance of the woodden platform [m]
+L_diag = (a**2 + b**2)**0.5; # Digonal distance of the woodden platform [m]
 
-J_wh = 0.025 ; # Moment of inertia of the wheel and motor rotating parts [kg.m2] obtained by experiment
+J_y = 2*(m_plat*a*a/12 + m_plat*(0.5*L1)**2) + 4*(m_bolt*(L1**2)/12 + m_bolt*(0.5*a)**2);
 
-J_mot_y = 2*J_wh # It is assumed that the I_y of motor stator = 2 * (I of wheel + gearbox)
+J_z = 2*(m_plat*(a*a + b*b)/12) + 4*(m_bolt * (0.5*L_diag)**2);
 
-def get_Jy():
-    
-    '''
-    This function is used for calculating the moment of inertia of the
-    robot main body about the Y axis or the axle. 
-    '''
-    J_lp = m_plat*a*a/12 + m_plat*d1**2 # I for lower platform about the axle
-    
-    J_up = m_plat*a*a/12 + m_plat*(L1+d1)**2 # I for upper platform about the axle
-    
-    J_bolt = (m_bolt*L1**2)/12 + m_bolt*(L1/2+d1)**2 + m_bolt*(a/2)**2
-    
-    J_y_total = J_lp + J_up + 4*J_bolt + 2*J_mot_y
-    
-    return J_y_total
+J_wh = 0.5*m_wh * r**2; # Moment of inertia of wheel + motor through its axis i.e. 0.5*mr^2 [kgm2] 
 
-def get_Jz():
-    
-    '''
-    This function is used for calculating the moment of inertia of the
-    robot main body about the Z axis . 
-    '''
-    J_lp = m_plat*(a*a + b*b)/12 # I for 1 platform about Z axis    
-  
-    J_bolt = m_bolt * (0.5*L_diag)**2
-    
-    J_mot_z = m_mot * (0.5*b)**2
-    
-    J_z_total = 2*J_lp + 4*J_bolt + 2*J_mot_z
-    
-    return J_z_total
+l = (m_plat*d1+4*m_bolt*(0.5*L1+d1)+m_plat*(L1+d1))/m_0 ; # Distance of vertical CG of the body from axle [m]
 
-J_y = get_Jy() # Moment of inertia of the main body about its principle Y axis
+g = 9.8; # Acceleration due to gravity [m/s2]
 
-J_z = get_Jz() # Moment of inertia of the main body about its principle Z axis
+J5 = J_z + 0.75*m_wh*b**2 + 0.5*m_wh*r*r; # Moment of inertia
 
-l = (m_plat*d1+4*m_bolt*(0.5*L1+d1)+m_plat*(L1+d1))/m_0; # Distance of vertical CG of the body from wheel centre [m]
+M = 2*m_0*m_wh*l*l*r*r + 2*J_wh*m_0*l**2 +  J_y*m_0*r**2 + 2*J_y*m_wh*r**2 + 2*J_wh*J_y;
 
-g = 9.8 # Acceleration due to gravity [m/s2]
+def lqr(A,B,Q,R):
+    """Solve the continuous time lqr controller.
+     
+    dx/dt = A x + B u
+     
+    cost = integral x.T*Q*x + u.T*R*u
+    """
+    #ref Bertsekas, p.151
+ 
+    #first, try to solve the ricatti equation
+    X = np.matrix(scipy.linalg.solve_continuous_are(A, B, Q, R))
+     
+    #compute the LQR gain
+    K = np.matrix(scipy.linalg.inv(R)*(B.T*X))
+     
+    eigVals, eigVecs = scipy.linalg.eig(A-B*K)
+     
+    return np.array(K), np.array(X), np.array(eigVals)
 
-J5 = J_z + 0.75*m_wh*b**2 + 0.5*m_wh*r*r # Moment of inertia
+def dlqr(A,B,Q,R):
+    """Solve the discrete time lqr controller.
+     
+     
+    x[k+1] = A x[k] + B u[k]
+     
+    cost = sum x[k].T*Q*x[k] + u[k].T*R*u[k]
+    """
+    #ref Bertsekas, p.151
+ 
+    #first, try to solve the ricatti equation
+    X = np.matrix(scipy.linalg.solve_discrete_are(A, B, Q, R))
+     
+    #compute the LQR gain
+    K = np.matrix(scipy.linalg.inv(B.T*X*B+R)*(B.T*X*A))
+     
+    eigVals, eigVecs = scipy.linalg.eig(A-B*K)
+     
+    return np.array(K), np.array(X), np.array(eigVals)
 
-M = 2*m_0*m_wh*l*l*r*r + 2*J_wh*m_0*l**2 +  J_y*m_0*r**2 + 2*J_y*m_wh*r**2 + 2*J_wh*J_y
 
 #%% A and B matrices
 
@@ -92,8 +98,44 @@ B[2,0] = r*(l*l*m_0+l*m_0*r+J_y)/M; B[3,1] = b/(2*r*J5)
 
 B_mot_torq = np.dot(B, np.array([[1 ,1],[1,-1]])) # Individual motor torques
 
-Q = np.zeros((4,4)); Q[0,0] = 100; Q[1,1] = 10; Q[2,2] = 1; Q[3,3] = 1; 
+Q = np.zeros((4,4));
+Q[0,0] = 1; # Penalty For Angular velocity
+Q[1,1] = 1; # Penalty For Angle
+Q[2,2] = 1; # Penalty For Linear velocity
+Q[3,3] = 1; # Penalty For turning velocity
 
-R = 10*np.array([[1,0],[0,1]])
+R = 100*np.array([[1,0],[0,1]])
 
-a, b, K = lqr(A, B_mot_torq, Q, R)
+K, _, _ = lqr(A,B_mot_torq,Q,R)  
+
+
+#%% Simulate system using ODE
+
+fs = 100
+
+dt = 1/fs
+
+t_vec = np.arange(0,t_sim,dt)
+
+IC = [0.1, 0.1, 0, 0] # Initial conditions [alpha_dot, alpha, V, theta] 
+    
+def msd(y,t):
+  
+    x_dd = np.dot((A - np.dot(B_mot_torq,K)),y)
+            
+    return x_dd
+
+X_vec = odeint(msd,IC,t_vec)
+
+#%%
+
+plt.plot(t_vec, X_vec[:,2])
+
+y_labs = [r'$\dot\Theta_{y}$ [rad/s]', r'$\Theta_{y}$ [rad]', 'V [m/s]', r'$\Omega_z$ [rad/s]']
+
+for i, j in enumerate(y_labs):
+    
+    plt.subplot(int(str(22)+str(i+1)))
+    
+    plt.plot(t_vec, X_vec[:,i])
+    plt.ylabel(j, fontsize = 16)
